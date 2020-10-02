a42aebe1b0435f11effd455f50d9bc976d9b20c0
unknown <kewel2010.mb@gmail.com>
2020-10-02 17:20:30 -0400

    ogg

--- src/whats-app.js
+++ src/whats-app.js
@@ -162,17 +162,17 @@ module.exports.recibeMessage = async(conn) => {
                 video.pipe(fs.createWriteStream(output, { flags: 'a' }))
                 video.on('end', async function() {
                     let buffer = fs.readFileSync(output)
-                    const options = { mimetype: Mimetype.mp4Audio }
-                    await conn.sendMessage(id, buffer, MessageType.audio, options);
-                    if (fs.existsSync(output)) {
-                        fs.unlinkSync(output)
-                    }
-                    // ffmpeg(output).toFormat('mp3').saveToFile('myaudio.mp3').on('end', async() => {
-                    //     let buffer = fs.readFileSync('myaudio.mp3')
 
-                    // }).on('error', (err) => {
-                    //     console.log("error", err);
-                    // })
+                    ffmpeg(output).toFormat('ogg').saveToFile('myaudio.ogg').on('end', async() => {
+                        let buffer = fs.readFileSync('myaudio.ogg')
+                        const options = { mimetype: Mimetype.ogg }
+                        await conn.sendMessage(id, buffer, MessageType.audio, options);
+                        if (fs.existsSync(output)) {
+                            fs.unlinkSync(output)
+                        }
+                    }).on('error', (err) => {
+                        console.log("error", err);
+                    })
                 })
 
             }
