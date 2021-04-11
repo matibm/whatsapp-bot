ead124ce95b39221711aa3b7fba51868f13eed61
matibm <kewel2010.mb@gmail.com>
2021-04-10 23:09:25 -0400

    s

---
+++ models/producto.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var producto = new Schema({
+
+    tipo: { type: Schema.Types.String },
+    marca: { type: Schema.Types.String },
+    precio: { type: Schema.Types.Number },
+    email: { type: Schema.Types.String },
+    pass: { type: Schema.Types.String },
+    cantidad: { type: Schema.Types.Number, default: 0 }
+
+
+}, { collection: 'productos' });
+
+module.exports = mongoose.model('Productos', producto);
--- src/whats-app.js
+++ src/whats-app.js
@@ -5,7 +5,9 @@ const MessageType = WA.MessageType;
 const client = new WhatsAppWeb()
 const fs = require('fs')
 const Usuario = require('./../models/usuario')
+const Producto = require('./../models/producto')
 const path = require('path')
+const bcrypt = require('bcrypt');
 
 const { env } = require('process');
 
@@ -23,7 +25,7 @@ module.exports.register = async(req, res) => {
 
     client.onReadyForPhoneAuthentication = ([ref, publicKey, clientID]) => {
         const str = ref + "," + publicKey + "," + clientID
-        console.log(str);
+            //g(str);
         res.status(200).json({
             ok: true,
             token: str
@@ -55,7 +57,7 @@ module.exports.conectApi = async(req, res) => {
         const cliente = new WhatsAppWeb()
 
         const conn = new WA.WAConnection
-        console.log("pasa", process.env.AUTH_INFO);
+            //g("pasa", process.env.AUTH_INFO);
 
         let auth = JSON.parse(process.env.AUTH_INFO)
         conn.loadAuthInfo(auth) // will load JSON credentials from file
@@ -91,12 +93,12 @@ module.exports.conectApi = async(req, res) => {
             this.conectApi()
         })
         this.recibeMessage(conn)
-        conn.requestPresenceUpdate('595971455095@s.whatsapp.net')
-        conn.on('user-presence-update', update => {
-            // will only be set if user has last seen enabled
-            console.log('user last seen is ')
-            console.log(update);
-        })
+            // conn.requestPresenceUpdate('595971455095@s.whatsapp.net')
+            // conn.on('user-presence-update', update => {
+            //     // will only be set if user has last seen enabled
+            //     console.log('user last seen is ')
+            //     console.log(update);
+            // })
     }
     // ENVIAR MENSAJES
 
@@ -113,11 +115,15 @@ module.exports.recibeMessage = async(conn) => {
     console.log("recibiendo mensajes", new Date());
     conn.on('message-new', async(m) => {
         let id = m.key.remoteJid
-        console.log("mi id", id);
+        console.log(m);
+        if (m.key.fromMe) {
+            console.log("retornando");
+            return
+        }
         if (!m.message || m.key.remoteJid.indexOf('status@broadcast') != -1) return // if there is no text or media message
         const messageType = Object.keys(m.message)[0] // get what type of message it is -- text, image, video
             // if the message is not a text message
-
+        console.log(messageType);
         if (messageType === MessageType.text) {
             let mensaje = m.message.conversation.toLocaleLowerCase()
 
@@ -144,15 +150,72 @@ module.exports.recibeMessage = async(conn) => {
                     email: arr[2]
                 }
                 let data = await Usuario.insertMany([obj])
-                console.log(data);
+                    //g(data);
+            }
+            if (mensaje.indexOf("producto-[") != -1) {
+
+                let msg = mensaje.replace('producto-[', '')
+                msg = msg.replace(']', '')
+
+                let arr = msg.split(', ')
+
+                let obj = {
+                        tipo: arr[0],
+                        marca: arr[1],
+                        precio: arr[2],
+                        email: arr[3],
+                        pass: arr[4]
+                    }
+                    //g(obj);
+                let data = await Producto.insertMany([obj])
+                    //g(data);
+                conn.sendMessage(id, `Producto creado✅`, MessageType.text);
+
             }
             if (mensaje.indexOf('consulta-usuario:') >= 0) {
                 let tel = mensaje.slice(17)
                 tel = tel.trim()
-                console.log(tel);
+                    //g(tel);
                 let user = await Usuario.findOne({ tel: tel }).select('tel nombre email')
                 delete user._id
-                conn.sendMessage(id, `Nombre: ${user.nombre}\nEmail: ${user.email}`, MessageType.text);
+                conn.sendMessage(id, `Nombre: ${user.nombre}\nEmail: ${user.email}`, MessageType.text, );
+
+            }
+            if (mensaje.indexOf('lista') >= 0) {
+                let tel = mensaje.slice(17)
+                tel = tel.trim()
+                console.log(tel);
+                let productos = await Producto.find()
+
+                let txt = `Responda a este mensaje con el numero del servicio que desea:\n`
+                for (let i = 0; i < productos.length; i++) {
+                    const element = productos[i];
+
+                    txt +=
+                        `${i+1}.
+  tipo: ${element.tipo},
+  marca: ${element.marca},
+  precio: ${element.precio},
+  id:${i+1}${element._id}
+
+
+`
+                }
+
+
+                let hash = bcrypt.hashSync(id, 10)
+                txt += `token:${hash}`
+                conn.sendMessage(id, txt, MessageType.text);
+
+            }
+            if (mensaje.indexOf('nuevo-producto') >= 0) {
+                let txt = `Por favor mande nuevo producto \n En el siguiente formato: \n [tipo, marca, precio, email, pass]`
+                conn.sendMessage(id, txt, MessageType.text);
+
+            }
+            if (mensaje.indexOf('-r') >= 0) {
+                let txt = `Por favor registre sus datos \n En el siguiente formato: \n [nombre, marca, precio, email, pass]`
+                conn.sendMessage(id, txt, MessageType.text);
 
             }
         }
@@ -175,6 +238,41 @@ module.exports.recibeMessage = async(conn) => {
                 conn.sendMessage(id, texto, MessageType.text);
             }
         }
+        if (messageType === MessageType.extendedText) {
+            try {
+                let mensaje = m.message.extendedTextMessage.text
+                let context = m.message.extendedTextMessage.contextInfo
+                if (!context.quotedMessage.conversation) {
+                    return
+                }
+
+                if (!isNaN(mensaje)) {
+                    // console.log("es un numero", mensaje);
+                    let textExtended = context.quotedMessage.conversation
+                    let token = textExtended.slice(textExtended.indexOf('token:') + 6)
+                    console.log(token);
+                    let permission = bcrypt.compareSync(id, token)
+                    if (!permission) {
+                        console.log("coinciden");
+                        return;
+                    }
+
+                    let id_product = textExtended.slice(
+                        textExtended.indexOf('id:' + mensaje) + 3 + mensaje.toString().length,
+                        textExtended.indexOf('id:' + mensaje) + 3 + mensaje.toString().length + 24)
+
+                    let product = await Producto.findById(id_product)
+
+                    let texto = `Ok elegiste ${product.marca} ${product.tipo}\nPaga: ${product.precio} Gs\nY hablamos🤙`
+                    conn.sendMessage(id, texto, MessageType.text);
+
+                }
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
+
 
     })
 
