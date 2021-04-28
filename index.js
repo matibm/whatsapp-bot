2be0ed19cf45600e74aafdca115b443da5cffc33
matibm <kewel2010.mb@gmail.com>
2021-04-27 23:24:24 -0400

    s

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/*', (req, res) => {
 // app.post('/whatsapp/sendmessage', whatsapp.sendMessage);
 // app.use('/search', youtube);
 
-mongoose.connection.openUri('mongodb://localhost:27017/whatsapp', (err, res) => {
+mongoose.connection.openUri(process.env.DB_URI, (err, res) => {
     if (err) throw err;
     console.log("Base de datos:  \x1b[32m%s\x1b[0m", ' online');
 })
